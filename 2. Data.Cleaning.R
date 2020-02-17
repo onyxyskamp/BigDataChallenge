0bbf69707f77c43e5c880f8bcc732a3cda5a7cd8
emeryu <60628741+emeryu@users.noreply.github.com>
2020-02-17 14:10:47 -0800

    Add files via upload

---
+++ 2. Data.Cleaning.R
@@ -0,0 +1,44 @@
+################################################################################
+
+#                                 DATA CLEANING                                #
+
+################################################################################
+#This section includes the code to clean the data so that it is useable for our 
+#analyses. 
+
+#First, make a data frame including only the relevent colums and check 
+#that it worked using the head function 
+bison<-data.frame(Bison$Elevation..ASL..m, Bison$Long, Bison$Lat,
+                  Bison$GISP2.Temp..IntCal13., Bison$DstL)
+head(bison)
+str(bison)
+
+# Get rid of the missing data points and check that it works using the structure 
+# function 
+biso <- na.omit(bison)
+str(biso)
+
+#Rename the columns to make them more usable and check that it works
+names(biso)[names(biso)== "Bison.Elevation..ASL..m"] <- "elev"
+names(biso)[names(biso)== "Bison.Long"] <- "long"
+names(biso)[names(biso)== "Bison.Lat"] <- "lat"
+names(biso)[names(biso)== "Bison.GISP2.Temp..IntCal13."] <- "temp"
+names(biso)[names(biso)== "Bison.DstL"] <- "dstl"
+head(biso)
+
+#Find the number of rows contained in dstl (needed to convert dstl to mass)
+str(biso$dstl)
+## 849 Rows
+
+#Create a new vector containing dstl converted to mass 
+#The formula for converting to mass was given in the paper
+#Check that it worked using the head function 
+mass <- (((biso$dstl[1:849])/11.43)^3)
+head(mass)
+
+#Add a new column to the data frame
+bis <- cbind(biso, mass)
+
+# Finally, check that the data frame is working and has all that you want 
+str(bis)
+
